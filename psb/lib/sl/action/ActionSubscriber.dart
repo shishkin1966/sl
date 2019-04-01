ba9eeb0144c0ce37fe73ed7f93a7d31fc3686afb
Oleg Shishkin <shishkin1966@gmail.com>
2019-04-01 12:30:47 +0300

    Доработка

--- psb/lib/sl/action/ActionSubscriber.dart
+++ psb/lib/sl/action/ActionSubscriber.dart
@@ -1,5 +1,10 @@
 import 'package:psb/sl/action/Action.dart';
 
 abstract class ActionSubscriber {
+  ///
+  /// Добавить действие на обработку
+  ///
+  /// @param action действие
+  ///
   void addAction(Action action);
 }
