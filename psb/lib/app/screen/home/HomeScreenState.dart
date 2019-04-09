a72273e45ab85ff1553aac16958ec0da898b3b82
Oleg Shishkin <shishkin1966@gmail.com>
2019-04-09 11:38:28 +0300

    Доработка

--- psb/lib/app/screen/home/HomeScreenState.dart
+++ psb/lib/app/screen/home/HomeScreenState.dart
@@ -206,6 +206,7 @@ class HomeScreenState extends WidgetState<HomeScreenWidget> {
                         new Material(
                           color: Color(AppColor.BlueMenu),
                           child: InkWell(
+                            highlightColor: Color(AppColor.BlueMenuPressed),
                             onTap: () {
                               SLUtil.getUISpecialist()
                                   .showToast('OnTapPayments');
@@ -234,6 +235,7 @@ class HomeScreenState extends WidgetState<HomeScreenWidget> {
                         new Material(
                           color: Color(AppColor.BlueMenu),
                           child: InkWell(
+                            highlightColor: Color(AppColor.BlueMenuPressed),
                             onTap: () {
                               _showSortMenu(context);
                               _bottomPosition = RolledBottomMenuHeight;
@@ -260,6 +262,7 @@ class HomeScreenState extends WidgetState<HomeScreenWidget> {
                         new Material(
                           color: Color(AppColor.BlueMenu),
                           child: InkWell(
+                            highlightColor: Color(AppColor.BlueMenuPressed),
                             onTap: () {
                               _showSelectMenu(context);
                               _bottomPosition = RolledBottomMenuHeight;
