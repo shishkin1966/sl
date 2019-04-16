7a18d544af622012f2505eafce8f308fe4b26f89
Oleg Shishkin <shishkin1966@gmail.com>
2019-04-16 20:33:33 +0300

    Доработка

--- psb/lib/sl/specialist/error/ErrorSpecialistImpl.dart
+++ psb/lib/sl/specialist/error/ErrorSpecialistImpl.dart
@@ -1,9 +1,13 @@
+import 'dart:io' as Io;
+
+import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:psb/common/StringUtils.dart';
 import 'package:psb/sl/AbsSpecialist.dart';
-import 'package:psb/sl/SL.dart';
 import 'package:psb/sl/SLUtil.dart';
 import 'package:psb/sl/specialist/error/ErrorSpecialist.dart';
-import 'package:psb/sl/specialist/ui/UISpecialistImpl.dart';
 
 class ErrorSpecialistImpl extends AbsSpecialist implements ErrorSpecialist {
   static const String NAME = "ErrorSpecialistImpl";
@@ -11,10 +15,32 @@ class ErrorSpecialistImpl extends AbsSpecialist implements ErrorSpecialist {
   static final ErrorSpecialistImpl _errorSpecialistImpl = new ErrorSpecialistImpl._internal();
 
   Logger _logger = Logger(NAME);
+  String _path;
 
   static ErrorSpecialistImpl get instance => _errorSpecialistImpl;
 
   ErrorSpecialistImpl._internal();
+
+  @override
+  void onRegister() async {
+    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    if (permission == PermissionStatus.granted) {
+      await _setPath();
+    } else {
+      Map<PermissionGroup, PermissionStatus> map =
+          await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+      if (map[PermissionGroup.storage] == PermissionStatus.granted) {
+        await _setPath();
+      }
+    }
+  }
+
+  Future _setPath() async {
+    Io.Directory dir = await getExternalStorageDirectory();
+    if (dir != null) {
+      _path = dir.path + "/psb_log.txt";
+    }
+  }
 
   @override
   int compareTo(other) {
@@ -29,24 +55,32 @@ class ErrorSpecialistImpl extends AbsSpecialist implements ErrorSpecialist {
   @override
   void onError(String source, Exception e) {
     _logger.severe(source, e, StackTrace.current);
-    if (SL.instance.exists(UISpecialistImpl.NAME)) {
-      SLUtil.uiSpecialist?.showErrorToast(e.toString());
+    SLUtil.uiSpecialist?.showErrorToast(e.toString());
+    _saveLog(e.toString());
+  }
+
+  void _saveLog(String text) {
+    if (!StringUtils.isNullOrEmpty(_path)) {
+      try {
+        Io.File(_path).writeAsStringSync(DateFormat("dd.MM.yyyy HH:mm:ss").format(DateTime.now()) + " " + text + "\n",
+            mode: Io.FileMode.append);
+      } catch (e) {
+        SLUtil.uiSpecialist?.showErrorToast(e.toString());
+      }
     }
   }
 
   @override
   void onErrorMessage(String source, String message) {
     _logger.severe(source, message, StackTrace.current);
-    if (SL.instance.exists(UISpecialistImpl.NAME)) {
-      SLUtil.uiSpecialist?.showErrorToast(message);
-    }
+    SLUtil.uiSpecialist?.showErrorToast(message);
+    _saveLog(message);
   }
 
   @override
   void onErrorDisplay(String source, Exception e, String message) {
     _logger.severe(source, e, StackTrace.current);
-    if (SL.instance.exists(UISpecialistImpl.NAME)) {
-      SLUtil.uiSpecialist?.showErrorToast(message);
-    }
+    SLUtil.uiSpecialist?.showErrorToast(message);
+    _saveLog(e.toString());
   }
 }
